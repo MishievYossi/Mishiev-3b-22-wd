25513abdc43fdaebd841410baa673c50c7e30b16
MishievYossi <134153083+MishievYossi@users.noreply.github.com>
2023-05-22 17:38:07 +0300

    Add files via upload

---
+++ Task-20.py
@@ -0,0 +1,19 @@
+import random
+
+my_list =[]
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+my_list.append(random.randint (1, 100))
+
+
+print(my_list)
+
+print(max(my_list))
+print(min(my_list))
