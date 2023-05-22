f8e7311f267e979392fc3e9609e9f57be7916b45
MishievYossi <134153083+MishievYossi@users.noreply.github.com>
2023-05-22 17:37:46 +0300

    Add files via upload

---
+++ Task-19.py
@@ -0,0 +1,9 @@
+my_list = []
+
+my_list.append((input("введите число: ")))
+my_list.append((input("введите число: ")))
+my_list.append((input("введите число: ")))
+my_list.append((input("введите число: ")))
+my_list.append((input("введите число: ")))
+
+print(my_list)
